73062a203d4bdbc69db2f3b9d1c12b27f22752df
kholo-chana <phaswane8@gmail.com>
2022-07-12 22:02:03 +0200

    initial commit

---
+++ springboot-security-rest-api/src/main/java/com/kholo/SpringbootSecurityRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.kholo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityRestApiApplication.class, args);
+	}
+
+}
---
+++ springboot-security-rest-api/src/test/java/com/kholo/SpringbootSecurityRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kholo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
